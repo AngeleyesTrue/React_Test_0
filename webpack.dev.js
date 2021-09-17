054a7b7e7922b7436e8853d3141387fed8f3fc0e
Angeleyes <angeleyes.true@gmail.com>
2021-09-17 13:48:53 +0000

    Package Update  and webpack version 5 check

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     devtool: "inline-source-map",
 
     devServer: {
-        inline: true,
+        //inline: true,
         hot: true
     },
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,14 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
